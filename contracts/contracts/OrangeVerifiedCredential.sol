c5d8dfb47f677934ab3e393b1118201990415e18
Samyak Sarnayak <samyak201@gmail.com>
2023-02-25 18:45:06 +0530

    feat: completed certificate contract

--- contracts/contracts/OrangeVerifiedCredential.sol
+++ contracts/contracts/OrangeVerifiedCredential.sol
@@ -2,137 +2,220 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract OrangeVerifiedCredential {
-    // schema: https://github.com/VC-Specs/vc-specs/blob/d1d227edad9a313d96ab3619c0b206efa610786f/common/CertificateBase.json#L6
-    struct CertificateBase {
-        string certificateId;
-        string issuer;
-        uint256 issuedOn;
-        uint256 signedDate;
-        uint256 validFrom;
-        uint256 validUntil;
-    }
-
-    // schema: https://github.com/VC-Specs/vc-specs/blob/d1d227edad9a313d96ab3619c0b206efa610786f/common/CertificateBase.json#L32
-    struct Subject {
+    struct CertificateType {
+        bool _isInit;
+        uint256 id;
         string name;
-        string mobile;
-        string email;
+        string data;
+        uint256 createdAt;
+        bool deleted;
+        address owner;
     }
 
-    // schema: https://github.com/VC-Specs/vc-specs/blob/d1d227edad9a313d96ab3619c0b206efa610786f/common/schema.org.json#L6
-    struct DefinedTerm {
-        string termCode;
-        string name;
-        string description;
-        string image;
-        string url;
-        string subjectOf;
-    }
+    CertificateType[] certificateTypes;
 
-    // schema: https://github.com/VC-Specs/vc-specs/blob/d1d227edad9a313d96ab3619c0b206efa610786f/skill/skill.json#L14
-    struct SkillCertificate {
-        CertificateBase base;
-        Subject subject;
-        string[] stringSkills;
-        DefinedTerm[] termSkills;
-    }
-
-    // schema: https://github.com/VC-Specs/vc-specs/blob/d1d227edad9a313d96ab3619c0b206efa610786f/common/Common.json#L5
-    struct Address {
-        string plot;
-        string street;
-        string landmark;
-        string locality;
-        string state;
-        string district;
-        string village;
-        string pincode;
-    }
-
-    // // schema: https://github.com/VC-Specs/vc-specs/blob/d1d227edad9a313d96ab3619c0b206efa610786f/work/powex.jsonld#L9
-    // struct ProofOfWorkExperience {
-    //     string issuer;
-    //     string legalName;
-    //     Address address_;
-    //     uint256 issuedOn;
-    //     Subject credentialSubject;
-    //     string lastJobTitle;
-    //     string worksFor;
-    //     OccupationalRole worksAs;
-    //     WorkLocation workLocation;
-    // }
-
-    // // schema: https://github.com/VC-Specs/vc-specs/blob/d1d227edad9a313d96ab3619c0b206efa610786f/work/powex.jsonld#L38
-    // struct PrivateProofOfWorkExperience {
-    //     string issuer;
-    //     uint256 issuedOn;
-    // }
-    struct ProofOfWorkExperience {
-        address issuer;
-        string legalName;
-        Address address_;
-        uint256 issuedOn;
-        Subject credentialSubject;
-        string lastJobTitle;
-        Organization worksFor;
-        OccupationalRole worksAs;
-        WorkLocation workLocation;
-        bytes proof;
-    }
-
-    struct PrivateProofOfWorkExperience {
-        address issuer;
-        uint256 issuedOn;
-        string isPartOf;
-        RoleCompensation roleCompensation;
-        RolePerformance rolePerformance;
-        AggregateRating rating;
-        bytes proof;
-    }
-
-    struct OccupationalRole {
-        string roleType;
-        string jobTitle;
-        uint256 startDate;
-        uint256 endDate;
-        string occupationalIndustry;
-        string occupationalCategory;
-    }
-
-    struct WorkLocation {
-        Address address_;
-        string geo;
-    }
-
-    struct RoleCompensation {
-        NumericRange monthlySalaryRange;
-        string salaryCurrency;
-    }
-
-    struct RolePerformance {
-        uint256 ratingCount;
-        uint256 ratingValue;
-        uint256 bestPossibleRating;
-        uint256 worstPossibleRating;
-        string author;
-    }
-
-    enum CertificateType {
-        Attendance,
-        Skill,
-        Work,
-        Reputation
-    }
-
-    // schema: https://github.com/VC-Specs/vc-specs/blob/d1d227edad9a313d96ab3619c0b206efa610786f/certificate.json
     struct Certificate {
-        string fullName;
-        string grades;
-        CertificateType certificateType;
-        string mobile;
-        string email;
-        string course;
-        string skill;
-        uint256 awardedOn;
+        bool _isInit;
+        uint256 id;
+        CertificateType type_;
+        uint256 createdAt;
+        // 0 means no expiry.
+        uint256 validity;
+        bool revoked;
+    }
+
+    Certificate[] certificates;
+
+    // CertificateTypeID => CertificateID[]
+    mapping(uint256 => uint256[]) certificatesWithType;
+
+    event CertificateTypeAdded(
+        uint256 id,
+        string name,
+        string data,
+        address indexed owner
+    );
+    event CertificateTypeDeleted(uint256 id);
+    event CertificateIssued(
+        uint256 indexed typeId,
+        uint256 indexed certificateId,
+        address indexed owner
+    );
+    event CertificateRevoked(
+        uint256 indexed typeId,
+        uint256 indexed certificateId,
+        address indexed revoker
+    );
+
+    // addCertificateType: Create a new certificate type.
+    function addCertificateType(
+        string memory _name,
+        string memory _data
+    ) public returns (uint256) {
+        require(bytes(_name).length > 0, "Name must not be empty");
+        require(bytes(_data).length > 0, "Data must not be empty");
+        uint256 id = certificateTypes.length;
+        certificateTypes.push(
+            CertificateType(
+                true,
+                id,
+                _name,
+                _data,
+                block.timestamp,
+                false,
+                msg.sender
+            )
+        );
+        emit CertificateTypeAdded(id, _name, _data, msg.sender);
+        return id;
+    }
+
+    // deleteCertificateType: Mark a certificate type as deleted.
+    function deleteCertificateType(uint256 _typeId) public {
+        require(
+            _typeId < certificateTypes.length,
+            "Invalid certificate type ID"
+        );
+        CertificateType storage certType = certificateTypes[_typeId];
+        require(
+            msg.sender == certType.owner,
+            "Only the certificate type owner can delete it"
+        );
+        certType.deleted = true;
+        emit CertificateTypeDeleted(_typeId);
+    }
+
+    function getAllCertificateTypes()
+        public
+        view
+        returns (CertificateType[] memory)
+    {
+        uint256 count = 0;
+        for (uint256 i = 0; i < certificateTypes.length; i++) {
+            if (certificateTypes[i]._isInit && !certificateTypes[i].deleted) {
+                count++;
+            }
+        }
+        CertificateType[] memory result = new CertificateType[](count);
+        uint256 index = 0;
+        for (uint256 i = 0; i < certificateTypes.length; i++) {
+            if (certificateTypes[i]._isInit && !certificateTypes[i].deleted) {
+                result[index] = certificateTypes[i];
+                index++;
+            }
+        }
+        return result;
+    }
+
+    // Get all certificate types (of msg.sender)
+    function getMyCertificateTypes()
+        public
+        view
+        returns (CertificateType[] memory)
+    {
+        uint256 count = 0;
+        for (uint256 i = 0; i < certificateTypes.length; i++) {
+            if (
+                certificateTypes[i]._isInit &&
+                certificateTypes[i].owner == msg.sender
+            ) {
+                count++;
+            }
+        }
+        CertificateType[] memory result = new CertificateType[](count);
+        uint256 index = 0;
+        for (uint256 i = 0; i < certificateTypes.length; i++) {
+            if (
+                certificateTypes[i]._isInit &&
+                certificateTypes[i].owner == msg.sender
+            ) {
+                result[index] = certificateTypes[i];
+                index++;
+            }
+        }
+        return result;
+    }
+
+    // addCertificate: Create a new certificate of a certain type.
+    function addCertificate(
+        uint256 _typeId,
+        uint256 _validity
+    ) public returns (uint256) {
+        require(
+            _typeId < certificateTypes.length,
+            "Invalid certificate type ID"
+        );
+        require(
+            certificateTypes[_typeId]._isInit &&
+                !certificateTypes[_typeId].deleted,
+            "Certificate type is not initialized or has been deleted"
+        );
+        CertificateType storage certType = certificateTypes[_typeId];
+        require(
+            msg.sender == certType.owner,
+            "Only the certificate type owner can add certificates of this type"
+        );
+        require(!certType.deleted, "Certificate type is deleted");
+        uint256 id = certificates.length;
+        certificates.push(
+            Certificate(true, id, certType, block.timestamp, _validity, false)
+        );
+        certificatesWithType[_typeId].push(id);
+        emit CertificateIssued(_typeId, id, msg.sender);
+        return id;
+    }
+
+    // revokeCertificate: Revoke a certificate.
+    function revokeCertificate(uint256 _certificateId) public {
+        require(_certificateId < certificates.length, "Invalid certificate ID");
+        Certificate storage certificate = certificates[_certificateId];
+        require(
+            msg.sender == certificate.type_.owner,
+            "Only the certificate type owner can revoke certificates of this type"
+        );
+        certificate.revoked = true;
+        emit CertificateRevoked(
+            certificate.type_.id,
+            _certificateId,
+            msg.sender
+        );
+    }
+
+    function getAllCertificatesOfType(
+        uint256 _typeId
+    ) public view returns (Certificate[] memory) {
+        require(
+            _typeId < certificateTypes.length,
+            "Invalid certificate type ID"
+        );
+        require(
+            certificateTypes[_typeId]._isInit &&
+                !certificateTypes[_typeId].deleted,
+            "Certificate type is not initialized or has been deleted"
+        );
+        uint256[] memory certificateIds = certificatesWithType[_typeId];
+        uint256 count = 0;
+        for (uint256 i = 0; i < certificateIds.length; i++) {
+            uint256 certificateId = certificateIds[i];
+            if (
+                certificates[certificateId]._isInit &&
+                !certificates[certificateId].revoked
+            ) {
+                count++;
+            }
+        }
+        Certificate[] memory result = new Certificate[](count);
+        uint256 index = 0;
+        for (uint256 i = 0; i < certificateIds.length; i++) {
+            uint256 certificateId = certificateIds[i];
+            if (
+                certificates[certificateId]._isInit &&
+                !certificates[certificateId].revoked
+            ) {
+                result[index] = certificates[certificateId];
+                index++;
+            }
+        }
+        return result;
     }
 }
