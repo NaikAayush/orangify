9132d42dd8e90baafb71ade35c1409b157ac5c2d
Aayush Naik <aayushnai@gmail.com>
2023-02-25 12:46:30 +0530

    feat: setup tailwind

--- app/src/pages/Tab1.tsx
+++ app/src/pages/Tab1.tsx
@@ -6,6 +6,7 @@ import {
   IonToolbar,
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
+import Toggle from "../components/Toggle";
 import "./Tab1.css";
 
 const Tab1: React.FC = () => {
@@ -22,6 +23,8 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Toggle />
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
