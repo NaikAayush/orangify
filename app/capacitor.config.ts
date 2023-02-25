3b68b33b6d73ac5b6f4561e823f92a57f22efb01
Aayush Naik <aayushnai@gmail.com>
2023-02-25 12:26:35 +0530

    feat: switch angular to react ionic

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'app',
-  webDir: 'www',
-  bundledWebRuntime: false
+  appId: "network.oranfify.app",
+  appName: "Orangify",
+  webDir: "build",
+  bundledWebRuntime: false,
 };
 
 export default config;
