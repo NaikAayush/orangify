232335ff2f95f0a0f21b455594823d599be7f457
Aayush Naik <aayushnai@gmail.com>
2023-02-26 15:32:52 +0530

    fix: enable cors

--- wallet-api/src/main.ts
+++ wallet-api/src/main.ts
@@ -5,6 +5,7 @@ import { jsonSerializableMiddleware } from './json-serializable.middleware';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(jsonSerializableMiddleware);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
